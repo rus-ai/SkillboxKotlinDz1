d21499e6c5b6c8b96500424d52890a94aac9e0c1
rus-ai <russian-ai@yandex.ru>
2020-12-11 01:20:16 +0300

    Ready

--- app/src/main/java/ru/wtw/skillboxkotlindz1/MainActivity.kt
+++ app/src/main/java/ru/wtw/skillboxkotlindz1/MainActivity.kt
@@ -7,15 +7,34 @@ import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity(), View.OnClickListener  {
+
+    private var longButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.buttonLong).setOnClickListener(this)
 
+        findViewById<Button>(R.id.buttonLeft).setOnClickListener(this)
+
+        val rightButton = findViewById<Button>(R.id.buttonRight)
+        rightButton.setOnClickListener{
+            customOnClick()
+        }
+
+        this.longButton = findViewById(R.id.buttonLong)
+        this.longButton!!.setOnClickListener {
+                Log.i("DZ1", "Long button pressed")
+        }
     }
 
     override fun onClick(v: View?) {
-        Log.i("DZ1", "Long button pressed")
-        TODO("Not yet implemented")
+        Log.i("DZ1", "Left button pressed")
     }
+
+    private fun customOnClick() {
+        Log.i("DZ1", "Right button pressed")
+    }
+
 }
+
+
