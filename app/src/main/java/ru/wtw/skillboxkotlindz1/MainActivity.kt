85672c0c26c5674959dee5246ec8c7d9226858a5
rus-ai <russian-ai@yandex.ru>
2020-12-10 23:21:32 +0300

    From command line

--- app/src/main/java/ru/wtw/skillboxkotlindz1/MainActivity.kt
+++ app/src/main/java/ru/wtw/skillboxkotlindz1/MainActivity.kt
@@ -3,9 +3,10 @@ package ru.wtw.skillboxkotlindz1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() implements OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
