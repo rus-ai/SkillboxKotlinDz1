21f3fc162cdc70b29744240e2c55dae277e55768
rus-ai <russian-ai@yandex.ru>
2020-12-11 00:10:53 +0300

    Long button

--- app/src/main/java/ru/wtw/skillboxkotlindz1/MainActivity.kt
+++ app/src/main/java/ru/wtw/skillboxkotlindz1/MainActivity.kt
@@ -2,11 +2,20 @@ package ru.wtw.skillboxkotlindz1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() implements OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.buttonLong).setOnClickListener(this)
 
+    }
+
+    override fun onClick(v: View?) {
+        Log.i("DZ1", "Long button pressed")
+        TODO("Not yet implemented")
     }
 }
